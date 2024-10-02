9a1dbf1dac0ef6b7386540ca630a5b070530e306
devPedro <devpedro37@gmail.com>
2024-10-01 22:08:57 -0300

    Inserindo a funcionalidade de cadastrar produto com o template html e funcionalidade de soma de produtos

--- JS/index.js
+++ JS/index.js
@@ -9,15 +9,57 @@ let itens = []
 document.querySelector('input[type=submit]')
 .addEventListener('click', ()=>{
     //CAPTURAR OS VALORES INSERIDOS NOS INPUTS
-    let nomeProduto = document.querySelector('input[name = nome_produto]').value
-    let precoProduto = document.querySelector('input[name = preco_produto]').value
+    let nomeProduto = document.querySelector('input[name = nome_produto]')
+    let precoProduto = document.querySelector('input[name = preco_produto]')
 
 
     //ADICIONAR OS VALORES NA LISTA
     itens.push({
-        nome: nomeProduto,
-        valor: precoProduto
+        //CAPTURANDO E INSERINDO OS VALORES DOS INPUTS
+        nome: nomeProduto.value,
+        valor: precoProduto.value
     })
-    //PRINTANDO O VALOR INSERIDO NO ARRAY
-    alert(itens[0].nome)
+    /*PRINTANDO O VALOR INSERIDO NO ARRAY teste
+    alert(itens[0].nome)*/
+
+    //CAPTURANDO div DO LISTA-PRODUTOS
+    let listaProdutos = document.querySelector('.lista-produtos')
+    let soma = 0 //VARIAVEL QUE VAI RECEBER O VALOR DA SOMA DOS PRODUTOS
+
+    listaProdutos.innerHTML = "" //limpando o valor da lista pra não repetir cadastro 
+
+
+    //MAPEANDO OS VALORES DO ARRAY E RECUPERANDO SEU VALOR
+    itens.map((valor)=>{
+
+        soma+= parseFloat(valor.valor) //A CADA VEZ QUE O LOOP REINICIAR, O VALOR DOS PRODUTOS SERÃO SOMADOS NA VARIÁVEL
+        listaProdutos.innerHTML +=`
+
+             <div class="lista-produtos-single">
+                <h3>${valor.nome}</h3>
+                <h3 class="preco"><span>R$ ${valor.valor}</span></h3>
+            </div>    
+        ` //OS NOMES NO TAMPLATE STRING SÃO DOS OBJETOS DA LISTA
+    })
+
+    //RESETANDO OS VALORES DOS INPUTS APÓS CADASTRAR
+    nomeProduto.value = ""
+    precoProduto.value = ""
+
+    //SETANDO AS CASAS DECIMAIS DA SOMA DOS PRODUTOS
+    soma = soma.toFixed(2)
+
+    //INSERINDO O VALOR DA SOMA NO HTML
+    let somaProdutos = document.querySelector('.soma-produto h2')
+    somaProdutos.innerHTML =+ ` ${soma}`
 })
+
+
+
+/* pesca para criar layout
+
+     <div class="lista-produtos-single">
+            <h3>Whey</h3>
+            <h3 class="preco"><span>R$ 20,00</span></h3>
+    </div>
+*/
